a5f8baedec69bd5e6b1bceff1b2eda922161daa8
lvpf <lvpengfei>
2019-03-27 17:40:47 +0800

    个人修改

--- personal_blog/queries/account.py
+++ personal_blog/queries/account.py
@@ -7,10 +7,10 @@ from graphene_sqlalchemy import SQLAlchemyObjectType,SQLAlchemyConnectionField
 
 class AccountQuery(graphene.ObjectType):
     current_user = Field(Account, description="当前用户")
-    users = SQLAlchemyConnectionField(Account,description="用户列表")
+    # users = SQLAlchemyConnectionField(Account,description="用户列表")
 
-    def resolve_current_user(self, info):
-        return Account.get_query(info).get(cu.id).first()
-
-    def resolve_users(self,info, args):
-        return Account.get_query(info)
+    # def resolve_current_user(self, info):
+    #     return Account.get_query(info).get(cu.id).first()
+    #
+    # def resolve_users(self,info, args):
+    #     return Account.get_query(info)
